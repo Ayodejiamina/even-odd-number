4f0ef6764667bdf346f9707319c1282179ab7e5d
Ayodeji Amina Ayomide <ayodejiamina@gmail.com>
2022-08-10 09:38:41 +0100

    numbers

---
+++ script.js
@@ -0,0 +1,19 @@
+let number = ""
+for (let i=1; i<=100; i++){
+    if(i%2 ==0){
+      number+= "This"+"" + i + "" +"is an even number"+"<br/>"
+    }else{
+        number+= "This" +i+ "" +"is an odd number"+"<br/>"
+    }
+}
+document.getElementById('even').innerHTML =number
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+
+
+// // multiple section
+// let mutiplication = ""
+// for(let i=1; 1<=100; i++){
+//     if(i*1 ==1){
+//         mutiplication += 'i' +'*' +1+'='
+//     }
+// }
